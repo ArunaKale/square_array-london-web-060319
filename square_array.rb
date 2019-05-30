2ff0a787c82aae0c4e85231d24423296f294dd61
AN K <a2nkale@gmail.com>
2019-05-30 21:34:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   # your code here
   new_array[]
-  array.each {|num| new_array.push}
+  array.each {|num| new_array.push num*num}
   self.map! {|num| num ** 2}
   array.squre!  
 end
